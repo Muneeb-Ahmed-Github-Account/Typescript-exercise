1e4f5a68ff45405e1276a8e254b03194cfec7c21
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-02 00:17:20 +0500

    Add files via upload

---
+++ Ex-14 Guest_List.ts
@@ -0,0 +1,2 @@
+let guestList = ["Ameen Alam", "Zia khan", "Daniyal Nagori"];
+guestList.forEach(oneGuest => console.log(`Asalam o Aleikum\n\n Mr. ${oneGuest},\n\nWould you like to Dinner with me?\n\nThankyou.`))
