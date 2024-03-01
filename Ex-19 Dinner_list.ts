94e44b4752080bc550ba9c5b54ff109c53be8fdf
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-02 00:28:41 +0500

    Add files via upload

---
+++ Ex-19 Dinner_list.ts
@@ -0,0 +1,5 @@
+let guestList = ["Ameen Alam", "Zia khan", "Daniyal Nagori"];
+//guestList.forEach(oneGuest => console.log(`Asalam o Aleikum\n\n Mr. ${oneGuest},\n\nWould you like to Dinner with me?\n\nThankyou.`))
+let guestNum: number = guestList.length
+
+console.log(`i invited ${guestNum} peoples for Dinner with me.`)
