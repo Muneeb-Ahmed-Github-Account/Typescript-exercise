b4d66eca0821b751c3b2518be0d6838e13fffe50
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-08 03:27:30 +0500

    Add files via upload

---
+++ ex-21 object.ts
@@ -0,0 +1,17 @@
+interface personalData {
+    Name : string;
+    Age : number;
+    Student : boolean;
+    Address : string ;
+    Email : string;
+}
+
+let personalData = {
+    Name : "Muneeb Ahmed",
+    Age  : 25 ,
+    Student : true ,
+    Address : "H/No: 123 st#03 Gulistan-e-Jouhar block 9 Karachi",
+    Email : "muneebkhuhro6427@gmail.com",
+};
+
+console.log(personalData);
