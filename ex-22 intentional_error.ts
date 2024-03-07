db9948b4ca1a7b9cb014370416bca48653a9ca14
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-08 03:29:39 +0500

    Add files via upload

---
+++ ex-22 intentional_error.ts
@@ -0,0 +1,8 @@
+//Making Array
+let intentionalError : string[] = ['Ghulam Qadir', 'Wajid', 'Muneeb','Mujahid','Kareem'];
+
+//check index of Array....Then i found undefined
+//console.log(intentionalError[5]);
+
+//print Array of Veriable and index NO so program run correctily.
+console.log(intentionalError[0]);
