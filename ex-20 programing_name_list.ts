65f7b8b17111e5db0e254500673db9f2fba035b7
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-08 03:17:46 +0500

    Add files via upload

---
+++ ex-20 programing_name_list.ts
@@ -0,0 +1,8 @@
+//Create Array of programing Languages.
+let listOfProgramingLanguages :string[] = ['Tailwind', 'Php', 'Javascript', 'Typescript', 'Python'];
+
+//Print Tital of Programing Languages and used back slash for new line.
+console.log("\nList of Programing Languages\n");
+
+// I used foreach method for print one by one Language Name.
+listOfProgramingLanguages.forEach(language => console.log(language));
