34cf296b2c17baa09245ee0f715ef4727c49b6d2
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-09 23:44:41 +0500

    Add files via upload

---
+++ ex-23 conditional_test.ts
@@ -0,0 +1,43 @@
+// Write a series of conditional
+
+//test NO: 1
+let car : string = 'subaru'
+console.log('car == "subaru"? i predict true.');
+console.log(car == 'subaru');
+
+//test No: 2
+console.log('car === "subaru"? i predict true.');
+console.log(car === 'subaru');
+
+//test No:3
+console.log('car != "bus"? i predict true.');
+console.log(car != 'bus');
+
+//test No: 4
+console.log('car != "cycle"? i predict true.');
+console.log(car != 'cycle');
+
+//test No: 5
+console.log('car greater then "motor cycle"? i predict true.');
+console.log(car > 'motor cycle');
+
+//test No: 6
+console.log('car less then "motor cycle"? i predict false.');
+console.log(car < 'motor cycle');
+
+//test No: 7
+console.log('car equal "motor cycle"? i predict false.');
+console.log(car == 'motor cycle');
+
+//test No:8
+let motorCycle :string = "motorCycle"
+console.log('motorcycle greater then "car"? i predict false.');
+console.log(motorCycle == 'car');
+
+//test No: 9
+console.log('car equal "train"? i predict false.');
+console.log(car == 'train');
+
+//test No: 10
+console.log('car equal "car"? i predict false.');
+console.log(car == 'car');
