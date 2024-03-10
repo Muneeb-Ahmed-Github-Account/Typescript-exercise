469834de0d78c3bb9097bb62bb5a53d15b04565c
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-10 22:08:53 +0500

    Add files via upload

---
+++ ex-25 alien_colors_1.ts
@@ -0,0 +1,11 @@
+let alienColor :string = "Grey";
+
+if (alienColor==="Grey") {
+console.log("player just earned 5 points !");
+}
+
+alienColor = "Green"
+
+if (alienColor==="Yellow") {
+    console.log("Try Again !");
+}
