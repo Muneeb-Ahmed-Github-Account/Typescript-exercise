fcb5c0d77946c436fe4651f5e471d0cbe15b8547
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-13 05:44:17 +0500

    Add files via upload

---
+++ 35_animals.ts
@@ -0,0 +1,5 @@
+let animals: string[] = ['Dog','Cat','Rabbit'];
+for(let animal of animals){
+    console.log(`A ${animal} would make a great pet.`);
+}
+console.log('Any of these animals would make a great pet!');
