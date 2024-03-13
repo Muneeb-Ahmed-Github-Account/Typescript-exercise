a95556d94acf7d48da7d8ee7755574df68d21432
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-13 05:37:58 +0500

    Add files via upload

---
+++ 31_no_users.ts
@@ -0,0 +1,9 @@
+let userNames : string[] = ['Admin', 'Shehroz', 'Shehzad', 'Faisal', 'Haneef'];
+if(userNames.length ===0 ){
+console.log("We find to need some users.");
+}
+else{
+userNames=[];
+
+    console.log("We removed all users.", userNames.length);
+}
