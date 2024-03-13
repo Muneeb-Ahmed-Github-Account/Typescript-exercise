9d9da920346e28029d25fe3ebd4d09e9f7014b05
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-13 05:34:17 +0500

    Add files via upload

---
+++ 29_favorite_fruits.ts
@@ -0,0 +1,11 @@
+let favorite_fruits : string[] = ['Mango', 'Apple','Bananas'];
+
+if(favorite_fruits.includes('Mango')){
+    console.log("I like Mango");
+}
+if(favorite_fruits.includes("Apple")){
+    console.log('I like Apple');
+}
+if(favorite_fruits.includes("Bananas")){
+    console.log('I like Bananas');
+}
