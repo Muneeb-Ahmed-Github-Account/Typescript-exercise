d1edfc0d9f55443098f8aa76159dd779158bd448
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-15 02:29:00 +0500

    Add files via upload

---
+++ 39_cities_names.ts
@@ -0,0 +1,7 @@
+function city_country(city: string, country: string){
+
+return (`${city}, ${country}`);
+}
+console.log(city_country('Lahore','Pakistan.'));
+console.log(city_country('jaddah','Saudi Arabia.'));
+console.log(city_country('Bait-ul-Muqadas','Palistine.')); 
