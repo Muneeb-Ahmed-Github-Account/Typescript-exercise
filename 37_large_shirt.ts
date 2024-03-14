d4deece066f629efa6e9951365dfa68eebda841d
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-15 02:26:52 +0500

    Add files via upload

---
+++ 37_large_shirt.ts
@@ -0,0 +1,7 @@
+function largeShirt(size: string = 'Large', text: string ='I Love Typescript !'){
+
+    console.log(`Creating a ${size} shirt with message: ${text}`);
+}
+largeShirt();
+largeShirt('Medium');
+largeShirt('Small', 'I Love Python!');
