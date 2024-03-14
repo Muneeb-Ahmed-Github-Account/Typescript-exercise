77f1a30392d43cd08dc87ba2974e856d1b9a7f7a
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-15 02:28:02 +0500

    Add files via upload

---
+++ 38_cities.ts
@@ -0,0 +1,8 @@
+function discribe_city(city:string,country:string= 'Default country.'){
+
+    console.log(`${city} is in ${country}`);
+}
+
+discribe_city('Karachi','Pakistan.');
+discribe_city('jaddah','Saudi Arabia.');
+discribe_city('Palistian');
