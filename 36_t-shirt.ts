e1ec8933bd411c8be7ef1749083235687dd7e386
Muneeb-Ahmed-Github-Account <161874476+Muneeb-Ahmed-Github-Account@users.noreply.github.com>
2024-03-15 02:25:46 +0500

    Add files via upload

---
+++ 36_t-shirt.ts
@@ -0,0 +1,7 @@
+function  make_shirt(size : string ,  text : string){
+
+    console.log(`Creating a ${size} shirt with a message: ${text}`);
+}
+make_shirt("Large" , "suitable for you");
+make_shirt("Medium" , "suitable for you");
+make_shirt("Small" , "suitable for you");
